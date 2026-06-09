086fbb7bf7af3782e7dfab864f8ae272bcbc6505
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-06-09 22:30:31 +0700

    step4

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -18,3 +18,25 @@ class Linkedlist
        {
         START = NULL;
        }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+        nodeBaru->next = START;
+        START = nodeBaru;
+        return;
+        }
+        
